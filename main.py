0b412f33b5db888a42f4d230d001578c050f65b3
Maksym Makarskyi <maksmakarskyi@gmail.com>
2024-10-19 23:00:15 +0200

    New model with a simple test

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('Hello Git and GitHub')
+import torch
+import torch.nn as nn
+
+
+test_input = torch.rand(1, 3, 28, 28)
+
+model = nn.Sequential(
+    nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
+    nn.BatchNorm2d(64),
+    nn.ReLU(),
+    nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+    nn.BatchNorm2d(128),
+    nn.ReLU(),
+    nn.Flatten(),
+    nn.Linear(7*7*128, 128),
+    nn.BatchNorm1d(128),
+    nn.ReLU(),
+    nn.Linear(128, 10),
+    nn.Softmax()
+)
+
+model.eval()
+
+print(model(test_input))
